fced0ec6a4ef9adacc86b1e1c931e6fe4262e31b
Matthew's Repository <huangsui91@163.com>
2014-03-21 00:42:05 +0800

    Copy the source code to my git repository, not implemented

---
+++ string/main.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "qh_string.h"
+
+
+int main(int argc, char* argv[])
+{
+    //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
+
+#ifdef WIN32
+    system("pause");
+#endif
+
+	return 0;
+}
+
