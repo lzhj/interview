4e418cde67ffea6cd9f62c94dddef7092d9ecf2c
Matthew's Repository <huangsui91@163.com>
2014-03-21 11:12:08 +0800

    implement qh::string

--- string/qh_string.cc
+++ string/qh_string.cc
@@ -7,31 +7,65 @@ namespace qh
     // TODO 将在这里写实现代码
 
     string::string()
-        : data_(NULL), len_(0)
     {
+        init();
     }
 
     string::string( const char* s )
     {
+        if(!s){
+           init();
+        }else{
+            int len = stlen(s);
+            data_ = new char[len+1];
+            strcpy(data_, s);
+            len_ = len;
+        }
     }
 
     string::string( const char* s, size_t len )
     {
+        if(!s || len == 0){
+            init();
+        }else{
+            size_t len_s = strlen(s);
+            if(len_s >= len){
+                data_ = new char[len+1];
+                strncpy(data_, s, len);
+                len_ = len;
+            }else{
+                data_ = new char[len_s+1];
+                strcpy(data_, s);
+                len_ = len_s;
+            }
+        }
     }
 
     string::string( const string& rhs )
     {
-
+        size_t len_rhs = strlen(rhs.data_);
+        data_ = new char[len_rhs+1];
+        strcpy(data_, rhs.data_);
+        len_ = len_rhs;
     }
 
     string& string::operator=( const string& rhs )
     {
+        if(this != &rhs){
+            delete[] data_;
+            data_ = 0;
+
+            size_t len_rhs = strlen(rhs.data_);
+            data_ = new char[len_rhs+1];
+            strcpy(data_, rhs.data_);
+            len_ = len_rhs;
+        }
         return *this;
     }
 
     string::~string()
     {
-
+        delete[] data_;
     }
 
     size_t string::size() const
@@ -41,16 +75,25 @@ namespace qh
 
     const char* string::data() const
     {
-        return NULL;
+        return data_;
     }
 
     const char* string::c_str() const
     {
-        return NULL;
+        return data_;
     }
 
-    char* string::operator[]( size_t index )
+    char& string::operator[]( size_t index )
     {
-        return NULL;
+        return data_[index];
+    }
+    const char& string::operator[](size_t index) const
+    {
+        return data_[index];
+    }
+    void string::init()
+    {
+        data_ = new char[0];
+        len_ = 0;
     }
 }
