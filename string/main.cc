591a09be5cd28d776c877a739025e496ec75d521
Matthew's Repository <huangsui91@163.com>
2014-03-21 14:43:25 +0800

    change main.cc

--- string/main.cc
+++ string/main.cc
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "qh_string.h"
+#include "gtest/gtest.h"
 
-
+extern bool string_equal_test(qh::string &, const char *);
 int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
@@ -11,6 +12,9 @@ int main(int argc, char* argv[])
     system("pause");
 #endif
 
-	return 0;
+    testing::InitGoogleTest(&argc, argv);
+    qh::string s;
+    //ASSERT_TRUE(string_equal_test(s, ""));
+	return RUN_ALL_TESTS();
 }
 
